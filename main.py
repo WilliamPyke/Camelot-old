649de1b9f72d958e29e7763279d8dce42b0c13d6
WilliamPyke <pykewilliam1@gmail.com>
2023-01-29 22:06:24 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,46 @@
+from PyPDF2 import PdfWriter, PdfReader
+import io
+import os
+from reportlab.pdfgen import canvas
+from reportlab.lib.pagesizes import letter
+from pdf2image import convert_from_path
+
+
+config = open('/Users/williampyke/Documents/UWI/2022:2023/Semester 2/ECNG 2000/config.txt', 'r')
+configLines = config.readlines()
+values = configLines[0].split()
+fullname = configLines[1].strip()
+date = configLines[2].strip()
+values = tuple(map(int, values))
+
+packet = io.BytesIO()
+can = canvas.Canvas(packet, pagesize=letter)
+can.drawString(values[0], values[1], fullname)
+can.drawString(values[2], values[3], date)
+can.drawImage("/Users/williampyke/Documents/UWI/2022:2023/Semester 2/ECNG 2000/signature.png", 125,87, width=120,height=40,mask=(0,0,0,0,0,0))
+can.save()
+
+file_path = os.path.dirname(os.path.abspath(__file__))
+print(file_path)
+
+#move to the beginning of the StringIO buffer
+packet.seek(0)
+
+# create a new PDF with Reportlab
+new_pdf = PdfReader(packet)
+# read your existing PDF
+existing_pdf = PdfReader(open("/Users/williampyke/Documents/UWI/2022:2023/Semester 2/ECNG 2000/PlagForm.pdf", "rb"))
+output = PdfWriter()
+
+# add the "watermark" (which is the new pdf) on the existing page
+page = existing_pdf.pages[0]
+page.merge_page(new_pdf.pages[0])
+output.add_page(page)
+# finally, write "output" to a real file
+output_stream = open("/Users/williampyke/Documents/UWI/2022:2023/Semester 2/ECNG 2000/out.pdf", "wb")
+output.write(output_stream)
+output_stream.close()
+
+pages = convert_from_path("/Users/williampyke/Documents/UWI/2022:2023/Semester 2/ECNG 2000/out.pdf", 500)
+for page in pages:
+    page.save('/Users/williampyke/Documents/UWI/2022:2023/Semester 2/ECNG 2000/out.png', 'PNG')
